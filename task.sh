a9e951abdc7049957dde928b0be39d2246c675e1
dream10201 <xiuxiu10201@gmail.com>
2022-11-30 11:40:06 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl -sS https://core.telegram.org/resources/cidr.txt >> ipcidr-text.txt
+curl -sS https://www.gstatic.com/ipranges/goog.json | perl -ne 'print "$1\n" if /Prefix\": \"(.+?)\"/' >> ipcidr-text.txt
 sort -u ipcidr-text.txt | uniq > .out
 cat .out > ipcidr-text.txt
 rm .out
