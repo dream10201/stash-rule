e42f2521fe504fad69a17b138d8a179fa11f5980
dream10201 <xiuxiu10201@gmail.com>
2022-12-01 14:19:22 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
+tmp=/tmp/.out
 curl -sS https://core.telegram.org/resources/cidr.txt >> ipcidr-text.txt
 curl -sS https://www.gstatic.com/ipranges/goog.json | perl -ne 'print "$1\n" if /Prefix\": \"(.+?)\"/' >> ipcidr-text.txt
 
 
-sort -u ipcidr-text.txt | uniq > .out
-cat .out > ipcidr-text.txt
+sort -u ipcidr-text.txt | uniq > $tmp
+cat $tmp > ipcidr-text.txt
 
-sort -u domain-text.txt | uniq > .out
-cat .out > domain-text.txt
+sort -u domain-text.txt | uniq > $tmp
+cat $tmp > domain-text.txt
 
 curl -sS https://purge.jsdelivr.net/gh/dream10201/stash-rule@master/ipcidr-text.txt
 curl -sS https://purge.jsdelivr.net/gh/dream10201/stash-rule@master/domain-text.txt
 
-
-rm .out
