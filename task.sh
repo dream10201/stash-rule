fee580fdc912ed3b5e335fb1202723a3395f0c2e
dream10201 <xiuxiu10201@gmail.com>
2022-12-01 14:16:03 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -6,11 +6,12 @@ curl -sS https://www.gstatic.com/ipranges/goog.json | perl -ne 'print "$1\n" if 
 
 sort -u ipcidr-text.txt | uniq > .out
 cat .out > ipcidr-text.txt
-rm .out
 
 sort -u domain-text.txt | uniq > .out
 cat .out > domain-text.txt
-rm .out
 
 curl -sS https://purge.jsdelivr.net/gh/dream10201/stash-rule@master/ipcidr-text.txt
 curl -sS https://purge.jsdelivr.net/gh/dream10201/stash-rule@master/domain-text.txt
+
+
+rm .out
