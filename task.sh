4e7a6162e84e015872306b8d31d6123dde43253c
dream10201 <xiuxiu10201@gmail.com>
2022-12-16 15:26:04 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -4,8 +4,9 @@ apt install whois -y
 tmp=/tmp/.out
 curl -sS https://core.telegram.org/resources/cidr.txt >> ipcidr-text.txt
 curl -sS https://www.gstatic.com/ipranges/goog.json | perl -ne 'print "$1\n" if /Prefix\": \"(.+?)\"/' >> ipcidr-text.txt
-//facebook
 whois -h whois.radb.net -- '-i origin AS32934' | grep ^route | perl -ne 'print "$1\n" if /route.+\s+(.+)/'>>ipcidr-text.txt
+whois -h whois.radb.net -- '-i origin AS13414' | grep ^route | perl -ne 'print "$1\n" if /route.+\s+(.+)/'>>ipcidr-text.txt
+whois -h whois.radb.net -- '-i origin AS4694' | grep ^route | perl -ne 'print "$1\n" if /route.+\s+(.+)/'>>ipcidr-text.txt
 
 sort -u ipcidr-text.txt | uniq > $tmp
 cat $tmp > ipcidr-text.txt
